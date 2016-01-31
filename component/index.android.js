9a73c279c18651afdb96b9be5e764c6f3ab5b28f
rpidanny <abhishekmaharjan1993@gmail.com>
2016-01-31 15:54:26 +0545

    onRegister and onNotification EventListener Working

--- component/index.android.js
+++ component/index.android.js
@@ -6,6 +6,10 @@ var {
 } = require('react-native');
 
 var NotificationAndroid = NativeModules.NotificationAndroid;
+var _notifHandlers = new Map();
+
+var DEVICE_NOTIF_EVENT = 'remoteNotificationReceived';
+var NOTIF_REGISTER_EVENT = 'remoteNotificationsRegistered';
 
 var NotificationComponent = function(){
 	this.initialPop = false;
@@ -23,6 +27,36 @@ NotificationComponent.prototype.schedule = function(details: Object){
 	NotificationAndroid.schedule(details);
 };
 
+NotificationComponent.prototype.addEventListener = function(type: String, handler: Function){
+	var listener;
+	if(type == 'notification'){
+		listener = DeviceEventEmitter.addListener(
+			DEVICE_NOTIF_EVENT,
+			function(notifyData){
+				var data = JSON.parse(notifyData.dataJSON);
+				handler(data);
+			}
+		);
+	}else if(type == 'register'){
+		listener= DeviceEventEmitter.addListener(
+			NOTIF_REGISTER_EVENT,
+			function(registrationInfo){
+				handler(registrationInfo.deviceToken);
+			}
+		);
+	}
+
+	_notifHandlers.set(handler,listener);
+}
+
+NotificationComponent.prototype.removeEventListener = function(type: String, handler: Function){
+	var listener = _notifHandlers.get(handler);
+	if(!listener){
+		return;
+	}
+	listener.remove();
+	_notifHandlers.delete(handler);
+}
 
 module.exports = {
 	state: false,
--- index.js
+++ index.js
@@ -1,5 +1,79 @@
 'use strict';
 
-var NotificationComponent = require( './component/index.android.js' );
+var NotificationComponent = require( './component' );
 
-module.exports = NotificationComponent.component;
+var AppState = NotificationComponent.state;
+var RNNotifications = NotificationComponent.component;
+
+var Platform = require('react-native').Platform;
+
+var Notifications = {
+	handler: RNNotifications,
+	onRegister: false,
+	onError: false,
+	onNotification: false,
+	loaded: false
+};
+
+Notifications.callNative = function(name: String, params: Array){
+	if(typeof this.handler[name] == 'function'){
+		if(typeof params !== 'array' &&
+			typeof params !== 'object'){
+			params = [];
+		}
+		return this.handler[name](...params);
+	}else{
+		return null;
+	}
+};
+
+Notifications.configure = function(options: Object){
+	if(typeof options.onRegister !== 'undefined'){
+		this.onRegister = options.onRegister;
+	}
+
+	if(typeof options.onNotification != 'undefined'){
+		this.onNotification = options.onNotification;
+	}
+
+	if(typeof options.senderID != 'undefined'){
+		this.senderID = options.senderID;
+	}
+
+	if(this.loaded == false){
+		this.callNative('addEventListener', ['register',this._onRegister.bind(this)])
+		this.callNative('addEventListener', ['notification', this._onNotification.bind(this)])
+	}
+
+	if(options.requestToken != false){
+		this.register();
+	}
+};
+
+Notifications.notify = function(details: Object){
+	this.handler.notify(details);
+	//todo ios implementation
+};
+
+Notifications._onRegister = function(token: String){
+	if(this.onRegister !== false){
+		this.onRegister(token);
+	}
+};
+
+Notifications._onNotification = function(data){
+	//todo ios implementation
+
+	if(this.onNotification !== false){
+		this.onNotification(data);
+	}
+
+};
+
+Notifications.register = function(){
+	//todo ios
+
+	return this.callNative('register',[this.senderID]);
+}
+
+module.exports = Notifications;
